cc2655969e6fcf7169a8128bb37ad7b47f5dfa33
vicky <lch82327@gmail.com>
2016-03-03 23:01:32 +0800

    add main function

--- BigNumber/main.cpp
+++ BigNumber/main.cpp
@@ -29,3 +29,12 @@ const BigNumber operator*(const BigNumber& lhs, const BigNumber& rhs);
 const BigNumber operator/(const BigNumber& lhs, const BigNumber& rhs);
 const BigNumber operator%(const BigNumber& lhs, const BigNumber& rhs);
 std::ostream& operator<<(std::ostream& os, const BigNumber& rhs);
+
+
+int main(){
+    uint8_t hex_a[] = "f1245ab3341ff3461818881767676819ee";
+    uint8_t hex_b[] = "ffa24387539639853800bbecbcb494990";
+    BigNumber a(3), b(5);
+    std::cout << "Hello world!" << std::endl;
+    return 0;
+}
