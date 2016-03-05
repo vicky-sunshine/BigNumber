7c1ca9b050c804044582fc71a119a5e8967fdfd8
vicky <lch82327@gmail.com>
2016-03-05 16:54:17 +0800

    rm redundant

--- BigNumber/main.cpp
+++ BigNumber/main.cpp
@@ -9,16 +9,5 @@ int main(int argc, char** argv){
   testing::InitGoogleTest(&argc, argv);
   RUN_ALL_TESTS();
   
-  //std::string hex_a =  "a";
-  BigNumber a(10);
-  BigNumber b(10);
-  std::cout << (a>b) << std::endl;
-  std::cout << (a<b) << std::endl;
-  std::cout << (a>=b) << std::endl;
-  std::cout << (a<=b) << std::endl;
-  std::cout << (a==b) << std::endl;
-  std::cout << (a!=b) << std::endl;
-  
-  
   return 0;
 }
